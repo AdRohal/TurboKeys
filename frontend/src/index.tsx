6261302a6242269411b66e5a50442ae149721e92
Adam Rohal <Kradamrohal@hotmail.com>
2025-07-12 03:26:49 +0100

    Adding Vercek Speed Insights

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -4,6 +4,7 @@ import './index.css';
 import App from './App';
 import { AuthProvider } from './contexts/AuthContext';
 import { ThemeProvider } from './contexts/ThemeContext';
+import { SpeedInsights } from "@vercel/speed-insights/react";
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
@@ -13,7 +14,10 @@ root.render(
   <React.StrictMode>
     <ThemeProvider>
       <AuthProvider>
-        <App />
+        <>
+          <App />
+          <SpeedInsights />
+        </>
       </AuthProvider>
     </ThemeProvider>
   </React.StrictMode>
